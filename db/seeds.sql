a7d609d04f6280eaedf46cea9a8d3b6e55ce1308
Cristina Zhang <rami0141@umn.edu>
2018-03-04 17:32:02 -0600

    mysql starting code

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+SELECT * FROM burgers_db.burgers;
+
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Trail Burger', true);
