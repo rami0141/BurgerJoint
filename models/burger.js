cb3d8b3bbcd42956bc18b6fba68b035fdc7dbed4
Cristina Zhang <rami0141@umn.edu>
2018-03-04 20:32:29 -0600

    code that interacts with the database

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+//Importing ORM to create functions that will interact with the database
+var orm = require("../config/orm.js");
+
+
+var burger = {
+  selectAll: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+
+   // The variables cols and vals are arrays.
+  //creates a burger in the database
+  insertOne: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+
+  //updates a burger in the database
+  updateOne: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burgersControllers.js).
+module.exports = cat;
